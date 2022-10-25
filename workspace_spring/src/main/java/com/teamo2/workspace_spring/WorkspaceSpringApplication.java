2c2c159408cbda25de20478b9091c64050a9ea92
John <john@teamo2.kr>
2022-10-25 12:26:44 +0900

    first commit

---
+++ workspace_spring/src/main/java/com/teamo2/workspace_spring/WorkspaceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.teamo2.workspace_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceSpringApplication.class, args);
+	}
+
+}
---
+++ workspace_spring/src/test/java/com/teamo2/workspace_spring/WorkspaceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamo2.workspace_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
