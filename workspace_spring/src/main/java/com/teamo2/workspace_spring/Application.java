f0b188b33486b2d9e265fd14e34dccb0d0c862f7
John <john@teamo2.kr>
2022-10-26 12:15:40 +0900

    (chore) 메인 어플리케이션 이름 변경 및 컨트롤러 예제 작성

--- workspace_spring/src/main/java/com/teamo2/workspace_spring/Application.java
+++ workspace_spring/src/main/java/com/teamo2/workspace_spring/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkspaceSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkspaceSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ workspace_spring/src/main/java/com/teamo2/workspace_spring/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.teamo2.workspace_spring.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
